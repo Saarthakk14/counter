98c2077f6f3eef9f66d3b459f748908a86595910
Saarthakk14 <122539546+Saarthakk14@users.noreply.github.com>
2023-01-26 08:52:19 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     count += 1
-    basic.showString("" + (count),50)
+    basic.showString("" + (count),90)
 })
 input.onButtonPressed(Button.AB, function () {
     count = 0
-    basic.showString("" + (count),50)
+    basic.showString("" + (count),90)
 })
 input.onButtonPressed(Button.B, function () {
     count += -1
-    basic.showString("" + (count),50)
+    basic.showString("" + (count),90)
 })
 let count = 0
 count = 0
-basic.showString("" + (count),50)
+basic.showString("" + (count),90)
