f24d9382547a5182a4130e1ef125e5230b39ac8a
Saarthakk14 <122539546+Saarthakk14@users.noreply.github.com>
2023-01-26 08:45:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    count += 1
+    basic.showString("" + (count))
+})
+input.onButtonPressed(Button.AB, function () {
+    count = 0
+    basic.showString("" + (count))
+})
+input.onButtonPressed(Button.B, function () {
+    count += -1
+    basic.showString("" + (count))
+})
+let count = 0
+count = 0
+basic.showString("" + (count))
