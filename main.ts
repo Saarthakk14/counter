61f0fbf48c57ec6b4f4f8211e67240d9a4ef7460
Saarthakk14 <122539546+Saarthakk14@users.noreply.github.com>
2023-01-26 08:50:20 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     count += 1
-    basic.showString("" + (count))
+    basic.showString("" + (count),50)
 })
 input.onButtonPressed(Button.AB, function () {
     count = 0
-    basic.showString("" + (count))
+    basic.showString("" + (count),50)
 })
 input.onButtonPressed(Button.B, function () {
     count += -1
-    basic.showString("" + (count))
+    basic.showString("" + (count),50)
 })
 let count = 0
 count = 0
-basic.showString("" + (count))
+basic.showString("" + (count),50)
